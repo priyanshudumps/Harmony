fab5d295e1c5280346d08ab79a7108b64a619802
priyanshur66 <priyanshuranjan0001@gmail.com>
2024-05-15 04:39:36 +0530

    add join button for brand

--- app/brand/page.js
+++ app/brand/page.js
@@ -1,17 +1,21 @@
 "use client";
 import { checkIsBrand, registerAsBrand } from "@/utils";
 import BrandNav from "../../components/BrandNav";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Brand() {
+  const [hasJoined, setHasJoined] = useState(false);
   useEffect(() => {
     const checkBrandValidity = async () => {
       const result = await checkIsBrand();
+      if (result) {
+        setHasJoined(true);
+      }
       console.log("is brand ?", result);
     };
 
     checkBrandValidity();
-  }, []);
+  }, [hasJoined]);
 
   return (
     <div>
