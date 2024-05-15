c53de35607b40957d3ceec30f4c67c9e917ad80f
priyanshur66 <priyanshuranjan0001@gmail.com>
2024-05-15 05:53:35 +0530

    add get promotions

--- app/promotions/page.js
+++ app/promotions/page.js
@@ -1,5 +1,14 @@
 "use client";
+import { useEffect } from "react";
 import { getAllEligiblePromotions } from "../../utils";
+
 export default function Promotions() {
+  useEffect(() => {
+    (async () => {
+      const res = await getAllEligiblePromotions();
+      console.log(res);
+    })();
+  }, []);
+
   return <div></div>;
 }
